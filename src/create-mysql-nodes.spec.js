76665d7ff995c01a1e8560f51df8abdc4471282c
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-05-11 12:21:46 +0800

    add test for one-to-one rel

--- src/create-mysql-nodes.spec.js
+++ src/create-mysql-nodes.spec.js
@@ -94,4 +94,70 @@ describe('createMysqlNodes', () => {
 
     expect(createNode.mock.calls).toMatchSnapshot();
   });
+
+  it('creates correct node for child entity in one-to-one relationship', () => {
+    const createNode = jest.fn();
+    const allSqlResults = [
+      {
+        name: 'countrycapital',
+        idFieldName: 'ID',
+        statement: 'SELECT * FROM countrycapital',
+        __sqlResult: [
+          { ID: 1, name: 'Kuala Lumpur', countryCode: 'MY' },
+          { ID: 2, name: 'Washington, D.C.', countryCode: 'US' },
+          { ID: 3, name: 'London', countryCode: 'UK' }
+        ],
+        parentName: 'country',
+        foreignKey: 'countryCode',
+        cardinality: 'OneToOne'
+      },
+      {
+        name: 'country',
+        idFieldName: 'Code',
+        statement: 'SELECT * FROM country',
+        __sqlResult: [
+          { Code: 'MY', name: 'Malaysia' },
+          { Code: 'US', name: 'United States' },
+          { Code: 'UK', name: 'United Kingdom' }
+        ]
+      }
+    ];
+
+    createMysqlNodes(allSqlResults[0], allSqlResults, createNode);
+
+    expect(createNode.mock.calls).toMatchSnapshot();
+  });
+
+  it('creates correct node for parent entity in one-to-one relationship', () => {
+    const createNode = jest.fn();
+    const allSqlResults = [
+      {
+        name: 'countrycapital',
+        idFieldName: 'ID',
+        statement: 'SELECT * FROM countrycapital',
+        __sqlResult: [
+          { ID: 1, name: 'Kuala Lumpur', countryCode: 'MY' },
+          { ID: 2, name: 'Washington, D.C.', countryCode: 'US' },
+          { ID: 3, name: 'London', countryCode: 'UK' }
+        ],
+        parentName: 'country',
+        foreignKey: 'countryCode',
+        cardinality: 'OneToOne'
+      },
+      {
+        name: 'country',
+        idFieldName: 'Code',
+        statement: 'SELECT * FROM country',
+        __sqlResult: [
+          { Code: 'MY', name: 'Malaysia' },
+          { Code: 'US', name: 'United States' },
+          { Code: 'UK', name: 'United Kingdom' }
+        ]
+      }
+    ];
+
+    createMysqlNodes(allSqlResults[1], allSqlResults, createNode);
+
+    expect(createNode.mock.calls).toMatchSnapshot();
+  });
 });
